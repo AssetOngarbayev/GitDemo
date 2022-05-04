5d8b100daf437513a7e9a21aeba3a39eb0d4ddbd
asset <aset-ong@yandex.ru>
2022-05-04 19:33:46 +0600

    Second commit

--- src/test/java/qaclickacademy/MavenJava/AppiumTest.java
+++ src/test/java/qaclickacademy/MavenJava/AppiumTest.java
@@ -8,6 +8,8 @@ public class AppiumTest {
 	@Test
 	public void NativeAppAndroid() {
 		System.out.println("NativeAppAndroid");
+		System.out.println("NativeAppAndroid1");
+		System.out.println("NativeAppAndroid2");
 	}
 	
 	@Test
