1286b3f7ea5e9f887dd703097b8c72a501a0d3b9
asset <aset-ong@yandex.ru>
2022-05-04 18:19:27 +0600

    first commit

---
+++ src/test/java/qaclickacademy/MavenJava/SeleniumTest.java
@@ -0,0 +1,15 @@
+package qaclickacademy.MavenJava;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+
+	@Test
+	public void browserAutomation() {
+		System.out.println("browserAutomation");
+	}
+	
+	public void elementsUI() {
+		System.out.println("elementsUI");
+	}
+}
